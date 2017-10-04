47a20512420cb8c89fea91b6e46fcca3e26364b2
Carlos Lima <carlos.lima@oracle.com>
2017-10-04 15:30:48 -0300

    Exercise 01 - Solution with update template and javascript

--- ex01-my-first-widget/widget/HelloUserWidget/js/helloUserWidget.js
+++ ex01-my-first-widget/widget/HelloUserWidget/js/helloUserWidget.js
@@ -1,5 +1,5 @@
 /**
- *  Hello User Widget
+ *  Hello User Widget v2
  *  my-first-extension/widget/HelloUserWidget/js/helloUserWidget.js
  */
 
@@ -15,12 +15,19 @@ define(
         "use strict";
         var widget;
         return {
+            isMorning: ko.observable(false),
             onLoad: function (widgetModel) {
                 widget = widgetModel;
                 console.log("-- helloUserWidget onLoad callback");
             },
             beforeAppear: function (page) {
                 console.log("-- helloUserWidget beforeAppear callback");
+                var date = new Date();
+                if (date.getHours()<12){
+                    widget.isMorning(true);
+                } else {
+                    widget.isMorning(false);
+                }
             }
         }
     }
